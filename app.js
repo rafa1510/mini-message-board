71b2385c5065fccf8beb702aade2b0239e6af91b
rafa1510 <34660086+rafa1510@users.noreply.github.com>
2024-08-19 01:09:05 -0400

    Set up messages render on index page

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import path from "node:path";
+import indexRouter from "./routes/indexRouter.js";
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", indexRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Mini Message Board - listening on port ${PORT}!`));
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,16 +1,22 @@
 import { Router } from "express";
 
-const indexRouter = Router()
+const indexRouter = Router();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
+
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+export default indexRouter;
